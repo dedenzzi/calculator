559dfd7487458f55169b4f07beaef16a01dbabc0
김형상 <angae1002@naver.com>
2017-01-17 17:26:48 +0900

    conflict1

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main(int argc, char* argv[])
 	int number1, number2, result;
 	char operator;
 	
-	printf("나다");
+	printf("first");
 	scanf("%d %c %d", &number1, &operator, &number2);
 	
 	switch (operator)
